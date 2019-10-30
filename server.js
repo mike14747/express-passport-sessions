d70aebd27d5b10cb3aadb500a8b99dd319f9b35e
mike14747 <mike4747@oh.rr.com>
2019-10-30 00:39:45 -0400

    still troubleshooting heroku deployment, restored server.js but without the session store

--- server.js
+++ server.js
@@ -5,69 +5,62 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// const passport = require('passport');
-// const session = require('express-session');
+const passport = require('passport');
+const session = require('express-session');
 // const sessionStore = require('./config/sessionStore');
 
-// app.use(express.static('views/css'));
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(session({
-//     secret: process.env.SESSION_SECRET,
-//     store: sessionStore,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         maxAge: 3600000,
-//     },
-// }));
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// app.get('/', checkAuthenticated, function (req, res) {
-//     res.sendFile(path.join(__dirname, 'views/index.html'));
-// });
-
-// app.get('/login', checkNotAuthenticated, function (req, res) {
-//     res.sendFile(path.join(__dirname, 'views/login.html'));
-// });
-
-// app.get('/register', checkNotAuthenticated, (req, res) => {
-//     res.sendFile(path.join(__dirname, 'views/register.html'));
-// });
-
-// app.get('/logout', checkAuthenticated, (req, res) => {
-//     req.logout();
-//     res.redirect('/login');
-// });
-
-// const controllers = require('./controllers');
-// app.use('/api', controllers);
-
-// app.get('*', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'views/index.html'));
-// });
-
-// function checkAuthenticated(req, res, next) {
-//     if (req.isAuthenticated()) {
-//         return next();
-//     }
-//     res.redirect('/login');
-// }
-
-// function checkNotAuthenticated(req, res, next) {
-//     if (req.isAuthenticated()) {
-//         return res.redirect('/');
-//     }
-//     next();
-// }
-
 app.use(express.static('views/css'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 3600000,
+    },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', checkAuthenticated, function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.get('/login', checkNotAuthenticated, function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
+app.get('/logout', checkAuthenticated, (req, res) => {
+    req.logout();
+    res.redirect('/login');
+});
+
+const controllers = require('./controllers');
+app.use('/api', controllers);
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/');
+    }
+    next();
+}
 
 app.listen(PORT, () => {
     // eslint-disable-next-line no-console
