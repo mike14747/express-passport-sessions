c5b18b99ff785f1f7c66a156780db6a05f814e08
mike14747 <mike4747@oh.rr.com>
2019-10-30 19:39:59 -0400

    starting to add things back into server1.js to see what makes it crash on heroku

--- server1.js
+++ server1.js
@@ -10,36 +10,36 @@ app.use(express.static('views/css'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login');
-}
+// function checkAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         return next();
+//     }
+//     res.redirect('/login');
+// }
 
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect('/');
-    }
-    next();
-}
+// function checkNotAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         return res.redirect('/');
+//     }
+//     next();
+// }
 
-app.get('/', checkAuthenticated, (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/login.html'));
 });
 
-app.get('/register', checkNotAuthenticated, (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/register.html'));
 });
 
-// app.get('/logout', checkAuthenticated, (req, res) => {
-//     req.logout();
-//     res.redirect('/login');
-// });
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/login');
+});
 
 const controllers = require('./controllers');
 app.use('/api', controllers);
