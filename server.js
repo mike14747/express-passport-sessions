01b9526d41750a7e24db26e1eef1dc1604e629d2
mike14747 <mike4747@oh.rr.com>
2019-10-30 00:49:46 -0400

    still haven't fixed the heroku deployment, so I changed server.js back to the way it was

--- server.js
+++ server.js
@@ -5,25 +5,25 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// const passport = require('passport');
-// const session = require('express-session');
-// const sessionStore = require('./config/sessionStore');
+const passport = require('passport');
+const session = require('express-session');
+const sessionStore = require('./config/sessionStore');
 
 app.use(express.static('views/css'));
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(session({
-//     secret: process.env.SESSION_SECRET,
-//     store: sessionStore,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         maxAge: 3600000,
-//     },
-// }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 3600000,
+    },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', checkAuthenticated, function (req, res) {
     res.sendFile(path.join(__dirname, 'views/index.html'));
