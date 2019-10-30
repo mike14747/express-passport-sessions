120d867e664a43ba5f48356851d91519b1f967d4
mike14747 <mike4747@oh.rr.com>
2019-10-30 19:24:42 -0400

    added a stripped down server1.js file and referenced it in package.json, plus changed the start script to use if-env to start a dev version using nodemon

---
+++ server1.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const { PORT } = process.env;
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('views/css'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server now listening on PORT ${PORT}!`);
+});
