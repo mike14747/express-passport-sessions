6ed315e16a1e8a86dc42d0a690d7e4d82339471e
mike14747 <mike4747@oh.rr.com>
2019-10-30 22:45:12 -0400

    I think I got to the bottom of the problem with this app crashing on heroku... the connection file wasn't setup properly, but I still need to enable multiple statements on the production connection

--- server.js
+++ server.js
@@ -25,11 +25,25 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', checkAuthenticated, function (req, res) {
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    }
+    res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        res.redirect('/');
+    }
+    next();
+}
+
+app.get('/', checkAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.get('/login', checkNotAuthenticated, function (req, res) {
+app.get('/login', checkNotAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, 'views/login.html'));
 });
 
@@ -37,31 +51,12 @@ app.get('/register', checkNotAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, 'views/register.html'));
 });
 
-app.get('/logout', checkAuthenticated, (req, res) => {
-    req.logout();
-    res.redirect('/login');
-});
-
 const controllers = require('./controllers');
 app.use('/api', controllers);
 
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
-
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login');
-}
-
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect('/');
-    }
-    next();
-}
 
 app.listen(PORT, () => {
     // eslint-disable-next-line no-console
