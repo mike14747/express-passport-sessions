54fc53c675b0d271e50654aa35e788d21b9617fb
mike14747 <mike4747@oh.rr.com>
2019-10-30 22:39:49 -0400

    starting to add things back into server1.js to see what makes it crash on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql2');
 
-// const connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PW,
-//     database: process.env.DB_NAME,
-//     multipleStatements: true,
-// });
+let connection;
 
-const connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.NODE_ENV === 'production') {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME,
+        multipleStatements: true,
+    });
+}
 
 connection.connect();
 
