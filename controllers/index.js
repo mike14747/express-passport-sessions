0025c270b9ba0176dc0250f3cf611a621ec21c72
mike14747 <mike4747@oh.rr.com>
2019-10-30 21:28:43 -0400

    starting to add things back into server1.js to see what makes it crash on heroku

--- controllers/index.js
+++ controllers/index.js
@@ -8,4 +8,16 @@ const router = require('express').Router();
 // const userController = require('./userController');
 // router.use('/user', userController);
 
+const db = require('../models/index.js');
+
+router.route('/').get((req, res) => {
+    res.status(200).send('Sending this from the /api/user route root!');
+});
+
+router.get('/id/:id', (req, res) => {
+    db.User.getUserById(req.params.id, (data) => {
+        res.json(data);
+    });
+});
+
 module.exports = router;
