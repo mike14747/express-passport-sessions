e4d0ae1b7a37604ab5ae52999e88288381070a91
mike14747 <mike4747@oh.rr.com>
2019-10-31 01:41:02 -0400

    switched back to server.js in package.json

--- config/sessionStore.js
+++ config/sessionStore.js
@@ -3,20 +3,14 @@ const connection = require('./connection');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 
-let sessionOptions;
+const sessionOptions = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+};
 
-if (process.env.NODE_ENV === 'production') {
-    sessionOptions = process.env.JAWSDB_URL;
-} else {
-    sessionOptions = {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PW,
-        database: process.env.DB_NAME,
-    };
-}
-
-const sessionStore = new MySQLStore(connection.promise);
+const sessionStore = new MySQLStore(sessionOptions, connection.promise);
 
 module.exports = sessionStore;
