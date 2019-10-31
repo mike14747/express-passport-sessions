a131ca99ef6dfd0bd3c6c864852b580dbf904602
mike14747 <mike4747@oh.rr.com>
2019-10-31 02:06:16 -0400

    deleted server1.js, removed JAWSDB_URL from sessionStore and connection since I added the env variables on heroku manually

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,13 @@
 const mysql = require('mysql2');
 
-let connection;
-
-if (process.env.NODE_ENV === 'production') {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PW,
-        database: process.env.DB_NAME,
-        multipleStatements: true,
-    });
-}
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+    multipleStatements: true,
+});
 
 connection.connect();
 
