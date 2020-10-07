79aead4fc023914fae170b4b207ad0ddc36aeb22
mike14747 <mike4747@oh.rr.com>
2020-10-07 16:40:36 -0400

    Update to authController.js.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const passport = require('../passport/passportFunctions');
 const User = require('../models/user');
-
 const bcryptjs = require('bcryptjs');
 
 router.route('/').get((req, res) => {
